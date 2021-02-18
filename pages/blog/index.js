b56b1aee44729fa06d1187f3716dbcafacf9a73d
TayteStokes <taytestokes9328@gmail.com>
2021-02-17 17:14:08 -0700

    removes references to chakra ui

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,25 +1,9 @@
 import React from 'react'
-import { Box, Text, useColorModeValue } from '@chakra-ui/react'
-import PageLayout from 'layouts/PageLayout'
+
 import { getAllBlogPosts } from 'utils/blog'
 
-import BlogPostCard from 'components/BlogPostCard'
-
 const BlogPage = ({ blogPosts }) => {
-  const textColor = useColorModeValue('gray.500', 'whiteAlpha.900')
-
-  return (
-    <PageLayout>
-      <Text fontWeight="bold" fontSize={['4xl', '6xl']}>
-        Blog
-      </Text>
-      <Box>
-        {blogPosts.map((post) => (
-          <BlogPostCard post={post} />
-        ))}
-      </Box>
-    </PageLayout>
-  )
+  return <></>
 }
 
 export async function getStaticProps() {
